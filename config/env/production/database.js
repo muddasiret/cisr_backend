8753223176e4a9f578144661ba3f799ad2e4afb9
muddasir <muddasir.e@itilite.com>
2024-02-28 15:18:48 +0530

    server,db update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,22 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+// path: ./config/env/production/database.js
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};
